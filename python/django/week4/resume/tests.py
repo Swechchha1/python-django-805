34b0ec0c169be79a7e068a080c06d5eb358dc01c
Swechchha1 <swechchhatiwari@gmail.com>
2018-02-21 13:20:51 -0500

    Added comments in tests.py

--- python/django/week4/resume/tests.py
+++ python/django/week4/resume/tests.py
@@ -8,24 +8,39 @@ class ResumeTest(TestCase):
     experience1 = None
 
     def setUp(self):
+        """
+        setUp needed to perform the tests, it is called before every test function.
+        """
         resume_test = Resume(first_name = "Swechchha", last_name = "Tiwari")
         resume_test.save()
 
     def test_full_name(self):
+        """
+        Test for get_full_name method defined in models.py
+        """
         r_test =  Resume.objects.first()
         self.assertEqual(r_test.get_full_name(), "Swechchha Tiwari")
 
     def test_last_name_first_name(self):
+        """
+        Test for get_last_name_first_name method defined in models.py
+        """
         r_test =  Resume.objects.first()
         self.assertEqual(r_test.get_last_name_first_name(), "Tiwari, Swechchha")
 
     def test_experience(self):
+        """
+        Test for get_experience method defined in models.py
+        """
         r_test = Resume.objects.first()
         actual = list(r_test.get_experience())
         expected = list(r_test.experience_set.all())
         self.assertEqual(actual,expected)
 
     def test_education(self):
+        """
+        Test for get_education method defined in models.py
+        """
         r_test = Resume.objects.first()
         actual = list(r_test.get_education())
         expected = list(r_test.education_set.all())
